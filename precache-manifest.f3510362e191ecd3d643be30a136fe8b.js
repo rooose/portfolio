13b7ed4eb9c62c6bdc53e1ee786f2af1ed90923b
Rose <rosehirigoyen@gmail.com>
2020-06-06 15:45:24 -0400

    Updates

---
+++ precache-manifest.f3510362e191ecd3d643be30a136fe8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7da4fd26fd12ee3096a672ecc51e060",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "453c3673dab4afef40d9",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/portfolio/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453c3673dab4afef40d9",
+    "url": "/portfolio/static/js/main.421b2783.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
