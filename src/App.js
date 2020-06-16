de667ab92f90e1918a437b1cbb936995677d7f85
Rose <rosehirigoyen@gmail.com>
2020-06-15 21:50:02 -0400

    just before major refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import LightboxGallery from "./LightboxGallery";
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -17,116 +18,122 @@ import './App.css';
 // work properly.
 
 export default function App() {
-  return (
-    <Router>
+    return (
+        <Router>
             <div className="App">
                 <header className="App-header">
                     <p>Juliette Hirigoyen</p>
                 </header>
             </div>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">All</Link>
-          </li>
-          <li>
-            <Link to="/animations">Animations</Link>
-          </li>
-          <li>
-            <Link to="/characters">characters</Link>
-          </li>
-          <li>
-            <Link to="/environments">characters</Link>
-          </li>
-          <li>
-            <Link to="/projects">projects</Link>
-          </li>
-          <li>
-            <Link to="/sketches">sketches</Link>
-          </li>
-        </ul>
+            <div>
+                <div className="Menu">
+                    <div className="table">
+                        <ul id="horizontal-list">
+                            <li>
+                                <Link to="/">All</Link>
+                            </li>
+                            <li>  •  </li>
+                            <li>
+                                <Link to="/animations">Animations</Link>
+                            </li>
+                            <li>  •  </li>
+                            <li>
+                                <Link to="/characters">characters</Link>
+                            </li>
+                            <li>  •  </li>
+                            <li>
+                                <Link to="/environments">characters</Link>
+                            </li>
+                            <li>  •  </li>
+                            <li>
+                                <Link to="/projects">projects</Link>
+                            </li>
+                            <li>  •  </li>
+                            <li>
+                                <Link to="/sketches">sketches</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
 
-        <hr />
-
-        {/*
+                {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
           matches the current URL. Use a <Switch> any time
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/animations">
-            <Animations />
-          </Route>
-          <Route path="/characters">
-            <Characters />
-          </Route>
-          <Route path="/environments">
-            <Environments />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/sketches">
-            <Sketches />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/animations">
+                        <Animations />
+                    </Route>
+                    <Route path="/characters">
+                        <Characters />
+                    </Route>
+                    <Route path="/environments">
+                        <Environments />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/sketches">
+                        <Sketches />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 // You can think of these components as "pages"
 // in your app.
 
 function Home() {
-  return (
-    <div>
-      <h2>Show all art</h2>
-    </div>
-  );
+    return (
+        <LightboxGallery photos={this.state.photos.slice(60, 75)} />
+    );
 }
 
 function Animations() {
-  return (
-    <div>
-      <h2>Show animations</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Show animations</h2>
+        </div>
+    );
 }
 
 function Characters() {
-  return (
-    <div>
-      <h2>Show characters</h2>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Show characters</h2>
+        </div>
+    );
 }
 
 function Environments() {
     return (
-      <div>
-        <h2>Show environments</h2>
-      </div>
+        <div>
+            <h2>Show environments</h2>
+        </div>
     );
-  }
+}
 
-  function Projects() {
+function Projects() {
     return (
-      <div>
-        <h2>Show projects</h2>
-      </div>
+        <div>
+            <h2>Show projects</h2>
+        </div>
     );
-  }
+}
 
-  function Sketches() {
+function Sketches() {
     return (
-      <div>
-        <h2>Show Sketches</h2>
-      </div>
+        <div>
+            <h2>Show Sketches</h2>
+        </div>
     );
-  }
+}
---
+++ src/LightboxGallery.js
@@ -0,0 +1,43 @@
+
+import React, { useState, useCallback } from 'react';
+import ReactDOM from 'react-dom';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+
+function LightboxGallery({ photos }) {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <h2>Using with a Lightbox component</h2>
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
+}
+
+export default LightboxGallery;
